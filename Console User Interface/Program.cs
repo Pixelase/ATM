9184f72c27c6f33f74397847a8d1a03197bd5d1e
Alexander Babaj <sustem34@gmail.com>
2015-05-19 17:38:53 +0300

    Create ATM.dll
    * Добавил ссылки на ATM.dll в каждый UI-проект;
    * Добавил ссылки на log4net.dll в каждый UI-проект;

--- Console User Interface/Program.cs
+++ Console User Interface/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ATM.Core;
 
 namespace Console_User_Interface
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            if (args == null) throw new ArgumentNullException("args");
+            var atm = new CashMachine(@"data.txt");
+
+            Console.WriteLine("Состояние счёта:");
+            Console.WriteLine(atm.Status() + '\n');
+
+            while (atm.Sum != 0)
+            {
+                Console.WriteLine("Введите нужную сумму:");
+                int userMoney;
+                int.TryParse(Console.ReadLine(), out userMoney);
+                while (userMoney > atm.Sum || userMoney <= 0)
+                {
+                    if (userMoney > atm.Sum || userMoney <= 0)
+                    {
+                        Console.WriteLine("Некорректный ввод\n\n" + "Введите нужную сумму:");
+                        int.TryParse(Console.ReadLine(), out userMoney);
+                    }
+                }
+
+                Console.WriteLine("Ваши деньги:");
+                foreach (var item in atm.WithdrawMoney(userMoney).Banknotes)
+                {
+                    Console.WriteLine("Купюра:" + item.Key.Nominal + " <-> Количество: " + item.Value);
+                }
+
+                Console.WriteLine("\nСостояние счёта:");
+                Console.WriteLine(atm.Status() + '\n');
+            }
+
+            Console.ReadKey();
         }
     }
 }
